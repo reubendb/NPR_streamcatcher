d33aa95734590efcf2cf5340da2cf1db78b3df13
Reuben D. Budiardja <reuben@budiardja.org>
2017-02-16 22:00:41 -0500

    Streamlining things.

--- streamrecord.sh
+++ streamrecord.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-StreamURL=$1
+StreamKeyword=$1
 ProgramName=$2
 ProgramDuration=$3
-SaveDirectory=$4
+SaveDirectory=$4/$2
 Today=`date +%Y-%m-%d`
 mkdir -p ${SaveDirectory} 
 cd ${SaveDirectory}
 FileName=${ProgramName}_${Today}.mp3 
 FileNameLatest=${ProgramName}_Latest.mp3
 
-
-#-- Shortname var
-GDriveRoot=GDriveUT:/drive/multimedia/podcast
-ACDRoot=ACD:/multimedia/podcast
-RCLONE_EXE=/usr/local/bin/rclone
+case $StreamKeyword in
+'WUOT'  ) StreamURL="http://playerservices.streamtheworld.com/api/livestream-redirect/WUOTFM.mp3" ;;
+'WUOT2' ) StreamURL="http://playerservices.streamtheworld.com/api/livestream-redirect/WUOTHD2.mp3" ;;
+*       ) StreamURL="http://playerservices.streamtheworld.com/api/livestream-redirect/WUOTFM.mp3" ;;
+esac
 
 # record
 mplayer $StreamURL -dumpstream -dumpfile $FileName > /dev/null 2>&1 &
@@ -21,12 +21,20 @@ Pid=$!
 sleep ${ProgramDuration}m
 kill ${Pid}
 
-/usr/local/bin/podcast_id3tag $FileName 
+#-- 
+ExecDir=$(dirname $0)
+${ExecDir}/podcast_id3tag $FileName 
 
-ProgramDir=$(basename $SaveDirectory)
-$RCLONE_EXE -q copy ${SaveDirectory} $GDriveRoot/${ProgramDir} && \
-  $RCLONE_EXE -q copy ${SaveDirectory} $ACDRoot/${ProgramDir} && \
-  rm -rf $SaveDirectory
+#-- If save remotely
+##-- Shortname var
+#GDriveRoot=GDriveUT:/drive/multimedia/podcast
+#ACDRoot=ACD:/multimedia/podcast
+#RCLONE_EXE=/usr/local/bin/rclone
+#
+#ProgramDir=$(basename $SaveDirectory)
+#$RCLONE_EXE -q copy ${SaveDirectory} $GDriveRoot/${ProgramDir} && \
+#  $RCLONE_EXE -q copy ${SaveDirectory} $ACDRoot/${ProgramDir} && \
+#  rm -rf $SaveDirectory
 
 #if [[ $FileName == "MorningEdition"* ]]; then 
 #  cp -f $FileName ../Latest/${FileNameLatest}
